6cc5346c39c7525cec888cabf3f7fc5c880ccb1f
CodePark-Pro <60905540+CodePark-Pro@users.noreply.github.com>
2022-05-01 14:41:06 +0900

    Add files via upload

---
+++ backg_p5.js
@@ -0,0 +1,32 @@
+let sketch3 = function(p){
+    p.setup = function setup() {
+        let c = p.createCanvas(p.windowWidth,p.windowHeight);
+        p.background(0);
+        //円の塗りつぶしは白(255)
+        p.fill(255);
+        //線の色は緑
+        p.stroke(0, 128, 0);
+        p.strokeWeight(2);
+        p.frameRate(30);
+    };
+    p.draw = function draw(){
+        if(p.frameCount%100==0){
+            p.stroke(p.random(128,255), p.random(128), p.random(128,255));
+        }
+        //線を引く(x座標は変化するのでi,y座標は一定、もう一点は固定x=240,y=60)
+        p.line((p.frameCount%100)*(p.windowWidth/100), 75, p.windowWidth/2, p.windowHeight/2);
+        //円を描く(x,y,幅、高さ)
+        p.ellipse((p.frameCount%100)*(p.windowWidth/100),75, 10, 10);
+        
+        
+        //下の部分を描く
+        //円を描く(x,y,幅、高さ)
+        p.line((p.frameCount%100)*(p.windowWidth/100), p.height-20, p.windowWidth/2, p.windowHeight/2);
+        p.ellipse((p.frameCount%100)*(p.windowWidth/100),p.height-20, 10, 10);
+    };
+    p.windowResized = function windowResized(){
+        p.resizeCanvas(p.windowWidth, p.windowHeight);
+    }
+  };
+  
+new p5(sketch3,"back_g");
