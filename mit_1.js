06ba8d2414a6e6f5593aa452c22d082567910e3c
yumiko <yumibon0722@gmail.com>
2022-05-02 16:53:24 +0900

    ver 0.0.6

--- mit_1.js
+++ mit_1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let sketch4 = function(p){
     p.setup = function setup() {
         p.createCanvas(200, 200);
@@ -13,24 +12,4 @@ let sketch4 = function(p){
         }
     };
 };
-
-
-=======
-let sketch4 = function(p){
-    p.setup = function setup() {
-        p.createCanvas(200, 200);
-        p.background(255);
-        };
-    p.draw = function draw() {
-        for(let i=0; i<p.width+25; i+=50){
-            for(j=0;j<p.height+25;j+=50){
-                p.fill(p.mouseX,p.mouseY,p.mouseX,127);
-                p.ellipse(i,j,30,30)
-            }
-        }
-    };
-};
-
-
->>>>>>> 752faa4b3e97976d7b96812695f5d2167fef26b7
 new p5(sketch4,"mit_1");
--- mit_2.js
+++ mit_2.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let sketch5 = function(p){
     p.setup = function setup() {
         p.createCanvas(200, 200);
@@ -10,19 +9,4 @@ let sketch5 = function(p){
     };
 };
 
-
-=======
-let sketch5 = function(p){
-    p.setup = function setup() {
-        p.createCanvas(200, 200);
-        p.background(255);
-        };
-    p.draw = function draw() {
-        p.stroke(0,p.mouseX,p.mouseY)
-        p.line  (p.mouseX,p.mouseY,p.width/2,p.height/2)
-    };
-};
-
-
->>>>>>> 752faa4b3e97976d7b96812695f5d2167fef26b7
 new p5(sketch5,"mit_2");
