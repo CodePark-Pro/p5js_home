867b21204415d74e6a3cc8a358bd1137acebf8bd
CodePark-Pro <60905540+CodePark-Pro@users.noreply.github.com>
2022-05-02 16:28:10 +0900

    Add files via upload

---
+++ mit_2.js
@@ -0,0 +1,13 @@
+let sketch5 = function(p){
+    p.setup = function setup() {
+        p.createCanvas(200, 200);
+        p.background(255);
+        };
+    p.draw = function draw() {
+        p.stroke(0,p.mouseX,p.mouseY)
+        p.line  (p.mouseX,p.mouseY,p.width/2,p.height/2)
+    };
+};
+
+
+new p5(sketch5,"mit_2");
